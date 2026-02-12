ea8cf0bc901ee54ee5a720c85b82215156653ab7
Vinay kumar Korakoppula <vinaykumar22ra1a6694@gmail.com>
2026-02-12 22:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (64.92%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0 ,high=nums.length-1;
+        int pos = nums.length;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                pos = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return pos;
+    }
+}
